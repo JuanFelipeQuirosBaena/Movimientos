9f25c1444671540530ad8fb84b67ad38f477abd1
JuanFelipeQuirosBaena <felipequiros416@gmail.com>
2026-06-26 01:01:58 -0500

    feat: registrar modelos en admin de Django

--- dcrm/movimientos/admin.py
+++ dcrm/movimientos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TipoMovimiento, Movimiento
 
-# Register your models here.
+
+@admin.register(TipoMovimiento)
+class TipoMovimientoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+
+@admin.register(Movimiento)
+class MovimientoAdmin(admin.ModelAdmin):
+    list_display = ('id_movimiento', 'tipo_movimiento', 'usuario', 'created_at', 'updated_at')
+    list_filter = ('tipo_movimiento', 'usuario')
+    search_fields = ('tipo_movimiento__nombre', 'usuario__username')
+    readonly_fields = ('created_at', 'updated_at')
