49062a9e2d32742d62cc686b3fcb7535c60bf529
JuanFelipeQuirosBaena <felipequiros416@gmail.com>
2026-06-26 00:59:19 -0500

    feat: crear vista de edición

--- dcrm/movimientos/views.py
+++ dcrm/movimientos/views.py
@@ -34,3 +34,17 @@ class MovimientoCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
     def form_invalid(self, form):
         messages.error(self.request, "Error al crear el movimiento. Revisa los datos ingresados.")
         return super().form_invalid(form)
+
+class MovimientoUpdateView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
+    model = Movimiento
+    form_class = MovimientoForm
+    template_name = 'movimientos/movimiento_update.html'
+    success_url = reverse_lazy('movimientos:list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Movimiento actualizado exitosamente.")
+        return super().form_valid(form)
+        
+    def form_invalid(self, form):
+        messages.error(self.request, "Error al actualizar el movimiento. Revisa los datos ingresados.")
+        return super().form_invalid(form)
